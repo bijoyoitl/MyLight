c5ef410143afdeb13d61780ba44f34a56df1be2e
bijoydeb <bijoycse2016@gmail.com>
2016-11-30 00:25:44 +0600

    runtime permission

--- app/src/main/java/com/optimalbd/mylight/MainActivity.java
+++ app/src/main/java/com/optimalbd/mylight/MainActivity.java
@@ -4,18 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView fLightImageView;
     PowerWork powerWork;
 
     private TextView batteryTextView;
-    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context arg0, Intent intent) {
             // TODO Auto-generated method stub
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fLightImageView = (ImageView) findViewById(R.id.fLightImageView);
-        powerWork = new PowerWork();
+        powerWork = new PowerWork(MainActivity.this);
 
         batteryTextView = (TextView) this.findViewById(R.id.batteryTextView);
         this.registerReceiver(this.mBatInfoReceiver,
@@ -40,11 +43,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (powerWork.getIsFlashOn()){
+                if (powerWork.getIsFlashOn()) {
                     powerWork.turnOffFlash();
                     powerWork.playSound(MainActivity.this);
                     fLightImageView.setImageResource(R.drawable.poweroff);
-                }else {
+                } else {
                     powerWork.turnOnFlash();
                     powerWork.playSound(MainActivity.this);
                     fLightImageView.setImageResource(R.drawable.poweron);
@@ -52,15 +55,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         powerWork.Destroy();
+        if (mBatInfoReceiver != null) {
+            unregisterReceiver(mBatInfoReceiver);
+        }
+    }
+
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == PowerWork.STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(MainActivity.this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/optimalbd/mylight/PowerWork.java
+++ app/src/main/java/com/optimalbd/mylight/PowerWork.java
@@ -1,15 +1,24 @@
 package com.optimalbd.mylight;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 public class PowerWork {
 
+    private Context context;
     private Camera camera;
     private boolean isFalshOn;
     private MediaPlayer mp;
+    public static int STORAGE_PERMISSION_CODE = 23;
 
     public boolean getIsFlashOn() {
         return this.isFalshOn;
@@ -18,6 +27,16 @@ public class PowerWork {
     public PowerWork() {
         this.isFalshOn = false;
         this.camera = Camera.open();
+    }
+
+    public PowerWork(Context context) {
+        this.context = context;
+        this.isFalshOn = false;
+        if (isReadStorageAllowed()){
+            this.camera = Camera.open();
+        }
+        requestStoragePermission(context);
+
     }
 
     public void turnOnFlash() {
@@ -47,7 +66,10 @@ public class PowerWork {
     }
 
     public void Destroy() {
-        this.camera.release();
+        if (camera!=null){
+            this.camera.release();
+        }
+
     }
 
     public void playSound(Context context) {
@@ -66,4 +88,32 @@ public class PowerWork {
         });
         mp.start();
     }
+
+    private boolean isReadStorageAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
+    }
+
+
+    //Requesting permission
+    private void requestStoragePermission(Context context){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,Manifest.permission.CAMERA)){
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions((Activity) context,new String[]{Manifest.permission.CAMERA},STORAGE_PERMISSION_CODE);
+    }
+
+
 }
